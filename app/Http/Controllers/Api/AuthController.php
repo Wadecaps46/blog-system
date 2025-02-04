c92adc34a91b4b929e7f02b514e7cadab666a944
Wilson De Caro P <143042094+Wadecaps46@users.noreply.github.com>
2025-02-04 20:16:10 +0000

    Creacion del archivo AuthController,php para la creacion y manejjo de registros de usuarios

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+// namespace App\Http\Controllers\Api;
+
+// use App\Http\Controllers\Controller;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Hash;
+// use App\Models\User;
+
+// class AuthController extends Controller
+// {
+//     public function register(Request $request)
+//     {
+//         $request->validate([
+//             'name' => 'required|string|max:255',
+//             'email' => 'required|string|email|max:255|unique:users',
+//             'password' => 'required|string|min:8|confirmed',
+//         ]);
+
+//         $user = User::create([
+//             'name' => $request->name,
+//             'email' => $request->email,
+//             'password' => Hash::make($request->password),
+//         ]);
+
+//         return response()->json([
+//             'user' => [
+//                 'name' => $user->name,
+//                 'email' => $user->email,
+//             ],
+//             'token' => $user->createToken('api')->plainTextToken,
+//         ], 201); // Código de estado 201 (Created)
+//     }
+// }
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'Usuario registrado exitosamente'], 201);
+    }
+}
